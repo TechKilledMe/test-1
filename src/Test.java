6536bc66a32cd167aa7ded5f0815e879ecb2a8a8
TechKilledMe <eberckmann@gmail.com>
2015-12-03 14:47:00 -0600

    Adding req. files
    Added Test.class and Test.java

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author Erick Berckmann
+ * @version 0.1 12/3/2015
+ *
+ */
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("This is a test for using GitHub.");
+
+	}
+
+}
