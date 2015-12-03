dd6db1eb49147a379b781577723c54e9ea8c4f9c
TechKilledMe <eberckmann@gmail.com>
2015-12-03 15:06:09 -0600

    Version 0.2b
    Minor edit

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 /**
  * 
  * @author Erick Berckmann
- * @version 0.2 12/3/2015
+ * @version 0.2b 12/3/2015
  *
  */
 
@@ -9,7 +9,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("This is a test for using GitHub.");
-		System.out.println("Hello world");
+		System.out.println("Hello world.");
+		System.out.println("Again this is a test for using GitHub.");
 
 	}
 
