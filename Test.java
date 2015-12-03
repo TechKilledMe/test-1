b2bef133ab9e4863226547ff270cad92ca61f617
TechKilledMe <eberckmann@gmail.com>
2015-12-03 15:02:17 -0600

    Version 0.2
    Edited Test.java

---
+++ Test.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author Erick Berckmann
+ * @version 0.2 12/3/2015
+ *
+ */
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("This is a test for using GitHub.");
+		System.out.println("Hello world");
+
+	}
+
+}
